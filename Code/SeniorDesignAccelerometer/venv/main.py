8109737352a309faf7f1f70967bccb631e0b8eab
Dana <dchea1116@gmail.com>
2020-11-12 00:20:49 -0500

    Done

--- Code/SeniorDesignAccelerometer/venv/main.py
+++ Code/SeniorDesignAccelerometer/venv/main.py
@@ -8,40 +8,61 @@ from queue import Queue
 
 def main():
     total = [0, 0, 0]                               #stores totals
-    fallthresh = 10                                 #if the average acceleration exceeds this fall is detected
-    axislimit = 2                                   #number of axis required to be over threshold (1-3) for fall to be detected   
+    fallthresh = 4.5                               #if the average acceleration exceeds this fall is detected
+    axislimit = 3                                   #number of axis required to be over threshold (1-3) for fall to be detected   
     buffersize = 4                       
     buf = Queue(maxsize = buffersize)               #creates buffer of desired size as a queue (first in first out)
     falldetected = False                            #stores if a fall is detected
 
-    while True:
-        print("%f %f %f" % accelerometer.acceleration)
-        print("Motion detected: %s" % accelerometer.events["motion"])
+    while not falldetected:
+        #print("%f %f %f" % accelerometer.acceleration)
+        #print("Motion detected: %s" % accelerometer.events["motion"])
         time.sleep(0.5)
         temp = accelerometer.acceleration           #gets accelerometer data
 
         #buffer is filling
         if (not buf.full()):                        #Buffer is not full
             buf.put(temp)                           #add item to queue
-            total += temp                           #add data to running totals
+            total[0] += abs(temp[0])                           #add data to running totals
+            total[1] += abs(temp[1])
+            total[2] += abs(temp[2])
         #buffer is full
         else:
-            total -= buf.get()                      #subtract oldest data from running totals
-            total += temp                           #add data to running totals
-            buf.put(temp)                           #adds item to queue
-
+            temprm = buf.get()
+            temprm = temprm
+            total[0] -= abs(temprm[0])                           #add data to running totals
+            total[1] -= abs(temprm[1])
+            total[2] -= abs(temprm[2])
+            buf.put(temp)                           #add item to queue
+            total[0] += abs(temp[0])                           #add data to running totals
+            total[1] += abs(temp[1])
+            total[2] += abs(temp[2])
+  
+        #print(total)
+        
         numAxisOverThresh = 0                       #stores the number of axis that are over the threshold
 
+        averages = [0, 0, 0]
+        i =0
+        
         for axistotal in total:                     #looks at each axis in the total
             axisavg = axistotal/buffersize          #computes average of each axis total         
             if(axisavg > fallthresh):               #if we have an axis over the treshold increase
                 numAxisOverThresh += 1              #increase the number of axis over threshold
-
+            averages[i] = axisavg
+            i += 1
+            
+        print(averages)
+            
         if(numAxisOverThresh >= axislimit):         #if too many axis are over the threshold
             falldetected = True                     #a fall is detected
         
         if(falldetected):
             print("FALL HAS BEEN DETECTED!!!!!")    #if fall detected print alert
+            falldetected = True
+            while(not buf.empty()):
+                buf.get()
+            total = [0,0,0]
 
 
 
@@ -49,6 +70,6 @@ if __name__ == "__main__":
     i2c = busio.I2C(board.SCL, board.SDA)  # prepare an I2C connection for our current boards SCL and SDA pins
     accelerometer = adafruit_adxl34x.ADXL345(i2c)  # instantiate the ADXL345 library into our “accelerometer” object
 
-    accelerometer.enable_freefall_detection(10, 25)
-    accelerometer.enable_motion_detection()
+    accelerometer.enable_freefall_detection(threshold=10, time=25)
+    accelerometer.enable_motion_detection(threshold=18)
     main()
